b628472435ffb3f4b5884d88fee5296fafa717b3
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-06-04 20:16:14 +0500

    feat: sign-up page made statefull and integrated with backend

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,8 +1,9 @@
 import { api } from "../config/axios";
+import { SignUpData } from "../types/auth";
 
-export const signUp = async (email: string, password: string) => {
+export const signUp = async (data: SignUpData) => {
   try {
-    const response = await api.post(`/auth/sign-up`, { email, password });
+    const response = await api.post(`/auth/sign-up`, data);
     return response.data;
   } catch (error) {
     throw error.response.data;
