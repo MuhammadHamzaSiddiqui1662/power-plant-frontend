6a65d349767bc7d7605c6aa1ea3f7d603302c8fe
hurm989 <hurmat.naz223@gmail.com>
2024-07-01 23:22:33 +0500

    added profile view section of all the roles

--- src/app/profile-view/page.js
+++ src/app/profile-view/page.js
@@ -8,8 +8,11 @@ const Switcher = dynamic(() => import("../componants/Switcher"));
 const Footer = dynamic(() => import("../componants/Footer"));
 
 import "./style.css";
+import { properties } from "../componants/Data";
+import Card from "../componants/Card";
 export default function ViewProfile(props) {
   // return <>hello</>
+  const [user, setUser] = useState(1);
   return (
     <>
       <Navbar />
@@ -18,7 +21,14 @@ export default function ViewProfile(props) {
           <section className={`relative mt-40`}>
             <div className="container">
               <p className={`heading font-bold leading-none text-center mb-12`}>
-                Broker Profile
+                {user == 0
+                  ? "Innovator"
+                  : user == 1
+                  ? "Investor"
+                  : user === 2
+                  ? "Broker"
+                  : ""}{" "}
+                Profile
               </p>
             </div>
           </section>
@@ -67,33 +77,39 @@ export default function ViewProfile(props) {
                 label={element}
               />
             ))}
-            <div className="flex">
-              <p className="text-customDarkBlue text-3xl mt-7 font-medium">
-                Certificates
-              </p>
-            </div>
-            <Grid container className="mt-1">
-              <Grid item xs={3}>
-                <div className="p-2">
-                  <img width="100%" src="/images/certificate.png" />
+            {user === 2 && (
+              <>
+                {" "}
+                <div className="flex">
+                  <p className="text-customDarkBlue text-3xl mt-7 font-medium">
+                    Certificates
+                  </p>
                 </div>
-              </Grid>
-              <Grid item xs={3}>
-                <div className="p-2">
-                  <img width="100%" src="/images/certificate.png" />
-                </div>
-              </Grid>
-              <Grid item xs={3}>
-                <div className="p-2">
-                  <img width="100%" src="/images/certificate.png" />
-                </div>
-              </Grid>
-              <Grid item xs={3}>
-                <div className="p-2">
-                  <img width="100%" src="/images/certificate.png" />
-                </div>
-              </Grid>
-            </Grid>
+                <Grid container className="mt-1">
+                  <Grid item xs={3}>
+                    <div className="p-2">
+                      <img width="100%" src="/images/certificate.png" />
+                    </div>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <div className="p-2">
+                      <img width="100%" src="/images/certificate.png" />
+                    </div>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <div className="p-2">
+                      <img width="100%" src="/images/certificate.png" />
+                    </div>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <div className="p-2">
+                      <img width="100%" src="/images/certificate.png" />
+                    </div>
+                  </Grid>
+                </Grid>
+              </>
+            )}
+
             <Grid container className="mt-5 mb-5">
               <Grid
                 item
@@ -115,11 +131,47 @@ export default function ViewProfile(props) {
                 </p>
                 <p className="text-customDarkBlue text-3xl mt-1">3</p>
               </Grid>
+              {user == 0 && (
+                <Grid item xs={12}>
+                  <p className="text-customDarkBlue text-3xl font-medium mt-6">
+                    Published IPs
+                  </p>
+                  <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 mt-3 gap-[30px] text-start">
+                    {properties.slice(0, 2).map((item, index) => (
+                      <Card
+                        key={index}
+                        name={item.name}
+                        id={item.id}
+                        description={item.description}
+                        year={item.year}
+                        category={item.category}
+                        price={item.price}
+                        patentNo={item.patentNo}
+                        image={item.image}
+                      />
+                    ))}
+                  </div>
+                </Grid>
+              )}
+              {user === 2 && (
+                <Grid item xs={12}>
+                  <div className="flex justify-start mt-5">
+                    <button
+                      type="submit"
+                      className="my-3 text-2xl btn bg-customGreen hover:bg-customGreen text-white rounded-md py-6 w-60 text-[32px]"
+                    >
+                      Hire / Contact
+                    </button>
+                  </div>
+                </Grid>
+              )}
             </Grid>
           </div>
         </Grid>
       </Grid>
-      <Footer />
+      <div className="mt-28">
+        <Footer />
+      </div>
       <Switcher />
     </>
   );
