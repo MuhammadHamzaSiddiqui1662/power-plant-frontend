1914acae57019d10fd9d65b9b644c8b64950168e
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-07-17 20:10:46 +0500

    chore: test page added for testing

---
+++ src/app/test/page.js
@@ -0,0 +1,27 @@
+"use client"; // This is a client component 👈🏽
+import React from "react";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+
+import "./style.css";
+import { useLoginMutation } from "../../services/auth/auth";
+const Navbar = dynamic(() => import("../componants/Navbar"));
+const Footer = dynamic(() => import("../componants/Footer"));
+
+export default function Pricing() {
+  const [login, { data, isLoading, isError }] = useLoginMutation();
+  return (
+    <>
+      <Navbar />
+      <section className="relative lg:py-24 py-16">
+        <div className="container">
+          <p className="mt-16 mb-12 text-5xl xs:text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-64px font-bold">
+            Test Page
+          </p>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
+}
