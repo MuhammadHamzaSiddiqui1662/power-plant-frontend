af37460e2339b817817f284b9e7d7555f409600f
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-07-31 20:53:16 +0500

    fix: eslint disabled during build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    cssModules: true,
-}
+  cssModules: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
