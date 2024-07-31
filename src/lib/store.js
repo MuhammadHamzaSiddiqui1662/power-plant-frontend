7ce29e71bc279eda9bc08285f189d04195631053
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-07-31 14:34:57 +0500

    feat: IP redux apis created

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { authApi } from "../services/auth/auth";
+import { ipApi } from "../services/ip/ip";
 import authReducer from "./features/authSlice";
 
 const rootReducer = combineReducers({
   [authApi.reducerPath]: authApi.reducer,
   auth: authReducer,
+  [ipApi.reducerPath]: ipApi.reducer,
 });
 
 export const makeStore = () => {
---
+++ src/services/ip/ip.js
@@ -0,0 +1,60 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BACKEND_URL } from "../../config/constants";
+
+export const ipApi = createApi({
+  reducerPath: "ipApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${BACKEND_URL}/ips`,
+    prepareHeaders: (headers, { getState }) => {
+      // By default, if we have a token in the store, let's use that for authenticated requests
+      const token = getState().auth.accessToken;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (build) => ({
+    getAll: build.query({
+      query: () => `/`,
+    }),
+    getIp: build.query({
+      query: (id) => `/${id}`,
+    }),
+    createIp: build.mutation({
+      query(body) {
+        return {
+          url: `/`,
+          method: "POST",
+          body,
+        };
+      },
+    }),
+    updateIp: build.mutation({
+      query(body) {
+        return {
+          url: `/${body.id}/details`,
+          method: "PUT",
+          body,
+        };
+      },
+    }),
+    publishIp: build.mutation({
+      query(body) {
+        return {
+          url: `/${body.id}/publish`,
+          method: "PUT",
+          body,
+        };
+      },
+    }),
+  }),
+});
+
+export const {
+  useCreateIpMutation,
+  useUpdateIpMutation,
+  usePublishIpMutation,
+  useGetAllQuery,
+  useGetIpQuery,
+} = ipApi;
