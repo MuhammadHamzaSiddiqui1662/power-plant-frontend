5b00eb29c6f7dd765db892cc8f1db801f9c16208
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-08-02 20:15:49 +0500

    fix: backend url for production changed

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   process.env.NODE_ENV === "production"
-    ? "https://localhost:3001/api/v1"
+    ? "https://power-plant-2a6a23ab8691.herokuapp.com/api/v1"
     : "http://localhost:3001/api/v1";
