421459f8bde6f9c65c2e369dcd9949c7cf5a83e8
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-08-03 10:59:48 +0500

    feat: persistance added to redux store for auth and current ip

--- src/lib/store.js
+++ src/lib/store.js
@@ -5,24 +5,47 @@ import { chatApi } from "../services/chat/chat";
 import { messageApi } from "../services/message/message";
 import authReducer from "./features/authSlice";
 import ipReducer from "./features/ipSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+// Persist configuration for auth slice
+const authPersistConfig = {
+  key: "auth",
+  storage,
+};
+
+// Persist configuration for auth slice
+const ipPersistConfig = {
+  key: "ip",
+  storage,
+  whitelist: ["currentIp"],
+};
 
 const rootReducer = combineReducers({
   [authApi.reducerPath]: authApi.reducer,
-  auth: authReducer,
+  auth: persistReducer(authPersistConfig, authReducer),
   [ipApi.reducerPath]: ipApi.reducer,
-  ip: ipReducer,
+  ip: persistReducer(ipPersistConfig, ipReducer),
   [chatApi.reducerPath]: chatApi.reducer,
   [messageApi.reducerPath]: messageApi.reducer,
 });
 
 export const makeStore = () => {
-  return configureStore({
+  const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware()
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+        },
+      })
         .concat(authApi.middleware)
         .concat(ipApi.middleware)
         .concat(chatApi.middleware)
         .concat(messageApi.middleware),
   });
+
+  const persistor = persistStore(store);
+
+  return { store, persistor };
 };
