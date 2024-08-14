4b6d696f4327a0ea1264334ba22efdffb026ff43
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-08-15 03:24:13 +0500

    fix: getChatObject moved to utils function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { UserType } from "../types/user";
+
 export const storeSessionToLocalStorage = (session: any) => {
   if (typeof window !== "undefined") {
     localStorage.setItem("access_token", session.access_token);
@@ -6,4 +8,15 @@ export const storeSessionToLocalStorage = (session: any) => {
     localStorage.setItem("refresh_token", session.refresh_token);
     localStorage.setItem("user", JSON.stringify(session.user));
   }
+};
+
+export const getChatObject = (chat, _id, _type) => {
+  if (_type == UserType.Innovator) {
+    chat.innovator = _id;
+  } else if (_type == UserType.Innvestor) {
+    chat.investor = _id;
+  } else if (_type == UserType.Broker) {
+    chat.broker = _id;
+  }
+  return chat;
 };
