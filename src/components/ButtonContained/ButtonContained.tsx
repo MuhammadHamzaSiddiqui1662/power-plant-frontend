b900f755d874054c9f3d9c10822d69583b07f661
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-08-17 04:39:42 +0500

    feat: size prop added to ButtonContained component

--- src/components/ButtonContained/ButtonContained.tsx
+++ src/components/ButtonContained/ButtonContained.tsx
@@ -8,14 +8,22 @@ interface Props
     HTMLButtonElement
   > {
   isLoading: boolean;
+  size: "medium" | "small";
 }
 
 const ButtonContained = forwardRef<HTMLButtonElement, Props>(
-  ({ isLoading, children, className, ...props }: Props, ref) => {
+  (
+    { isLoading, children, className, size = "medium", ...props }: Props,
+    ref
+  ) => {
     return (
       <button
         ref={ref}
-        className={`text-2xl btn bg-customGreen hover:bg-customGreen text-white rounded-md py-6 w-40 text-[32px] ${className}`}
+        className={`text-${
+          size === "small" ? "lg" : "2xl"
+        } btn bg-customGreen hover:bg-customGreen text-white rounded-md py-${
+          size === "small" ? "4" : "6"
+        } w-40 text-[${size === "small" ? "18" : "24"}px] ${className}`}
         {...props}
       >
         {isLoading ? <CircularProgress color="inherit" size={24} /> : children}
