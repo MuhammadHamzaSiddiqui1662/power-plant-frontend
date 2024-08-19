084128a5a8d3387befa849236ec019fea387b259
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-08-19 05:33:14 +0500

    feat: current investor tag added

--- src/app/componants/MyInvestorsPreview.js
+++ src/app/componants/MyInvestorsPreview.js
@@ -3,10 +3,12 @@ import React from "react";
 import { Avatar } from "@mui/material";
 import { useGetMyInvestorsQuery } from "../../services/hiring/hiring";
 import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
 
 export default function MyInvestorsPreview() {
   const router = useRouter();
   const { data: investors } = useGetMyInvestorsQuery();
+  const { currentInvestor } = useSelector((state) => state.hiring);
 
   return (
     <div className="avatar-div">
@@ -32,9 +34,20 @@ export default function MyInvestorsPreview() {
                   src="/images/client/01.jpg"
                 />
               </div>
-              <p className="text-base sm:text-sm md:text-lg lg:text-lg xl:text-2xl 2xl:text-2xl name">
+              <p className="text-base sm:text-sm md:text-lg lg:text-lg xl:text-2xl 2xl:text-2xl name w-full">
                 {investor.investor.name}
               </p>
+              {investor.investor._id === currentInvestor._id && (
+                <p
+                  className="leading-3 text-[12px] px-2 py-1 border rounded"
+                  style={{
+                    color: "#6BB955",
+                    borderColor: "#6BB955",
+                  }}
+                >
+                  Current
+                </p>
+              )}
             </div>
           ))
         ) : (
