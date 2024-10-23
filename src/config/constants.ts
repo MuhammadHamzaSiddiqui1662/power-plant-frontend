9720e139b2be1b6f3cc2b6d21aec403543646ff0
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-10-24 04:13:56 +0500

    fix: live backend url updated

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,11 +1,11 @@
 export const BACKEND_URL =
   process.env.NODE_ENV === "production"
-    ? "https://power-plant-2a6a23ab8691.herokuapp.com/api/v1"
+    ? "http://ec2-15-157-25-235.ca-central-1.compute.amazonaws.com/api/v1"
     : "http://localhost:3001/api/v1";
 
 export const BACKEND_SOCKET_URL =
   process.env.NODE_ENV === "production"
-    ? "https://power-plant-2a6a23ab8691.herokuapp.com"
+    ? "http://ec2-15-157-25-235.ca-central-1.compute.amazonaws.com"
     : "http://localhost:3001";
 
 export const CATEGORIES = {
